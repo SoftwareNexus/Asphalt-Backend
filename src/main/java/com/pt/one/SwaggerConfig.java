10e3ab73256d4586fae5c172701d3e5cbbb56ea1
SNOW <kancth03@gmail.com>
2023-05-18 06:31:57 +0900

    feat: Swagger 페이지 추가

---
+++ src/main/java/com/pt/one/SwaggerConfig.java
@@ -0,0 +1,26 @@
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "커뮤니티 API 명세서",
+        description = "로그인 및 회원가입 기능을 기반으로 둔 게시판",
+        version = "v1")
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("커뮤니티서비스 API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
--- src/main/java/com/pt/one/controller/LoginController.java
+++ src/main/java/com/pt/one/controller/LoginController.java
@@ -5,6 +5,12 @@ import com.pt.one.dto.LoginResDTO;
 import com.pt.one.dto.MemberDTO;
 import com.pt.one.service.JoinService;
 import com.pt.one.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +20,16 @@ public class LoginController {
     @Autowired
     MemberService memberService;
 
+    @Operation(summary = "로그인 요청", description = "로그인 정보로 서버에 로그인 요청을 보냅니다.",
+    tags = { "Member Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = LoginResDTO.class)))
+    })
     @PostMapping("/login")
-    public LoginResDTO login(@RequestBody LoginDTO loginDTO){
+    public LoginResDTO login(
+            @Parameter(description = "회원 아이디 및 비밀번호", required = true)
+            @RequestBody LoginDTO loginDTO){
         LoginResDTO loginResDTO = memberService.login(loginDTO);
         return loginResDTO;
     }
